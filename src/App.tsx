12518b63726087c3635f20e5610b080ebed187aa
OperinLabs <rajgoswami.co@gmail.com>
2026-09-10 21:40:02 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { AnimatePresence } from "framer-motion";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import Product from "./components/Product";
+import Pricing from "./components/Pricing";
 import BookDemoModal from "./components/BookDemoModal";
 
 function App() {
@@ -11,6 +13,8 @@ function App() {
     <div className="min-h-screen bg-bg font-inter">
       <Navbar onBookDemo={() => setDemoOpen(true)} />
       <Hero onBookDemo={() => setDemoOpen(true)} />
+      <Product />
+      <Pricing onBookDemo={() => setDemoOpen(true)} />
 
       <AnimatePresence>
         {demoOpen && <BookDemoModal onClose={() => setDemoOpen(false)} />}
