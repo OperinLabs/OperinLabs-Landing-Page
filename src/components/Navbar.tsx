77dabd18fecd7989b95170db2feef4a3be0411b3
OperinLabs <rajgoswami.co@gmail.com>
2026-09-10 13:59:29 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -29,7 +29,7 @@ export default function Navbar({ onBookDemo }: NavbarProps) {
             <img
               src="/logo.png"
               alt="OperinLabs"
-              className="h-34 w-34"
+              className="h-32 w-auto"
             />
           </a>
 
