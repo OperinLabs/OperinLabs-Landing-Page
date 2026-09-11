951092ea6b13c6ce46479a78043ce2b531355eb1
OperinLabs <rajgoswami.co@gmail.com>
2026-09-11 12:02:44 +0530

    Update PricingPage.tsx

--- src/pages/PricingPage.tsx
+++ src/pages/PricingPage.tsx
@@ -1,1 +1,13 @@
+import Pricing from "../components/Pricing";
 
+interface PricingPageProps {
+  onBookDemo: () => void;
+}
+
+export default function PricingPage({ onBookDemo }: PricingPageProps) {
+  return (
+    <div className="pt-20">
+      <Pricing onBookDemo={onBookDemo} />
+    </div>
+  );
+}
