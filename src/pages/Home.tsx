3bd1dfe6c6811839de5bcf67a2eb4abe83e45241
OperinLabs <rajgoswami.co@gmail.com>
2026-09-11 11:58:20 +0530

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,1 +1,15 @@
+import Hero from "../components/Hero";
+import Product from "../components/Product";
 
+interface HomeProps {
+  onBookDemo: () => void;
+}
+
+export default function Home({ onBookDemo }: HomeProps) {
+  return (
+    <>
+      <Hero onBookDemo={onBookDemo} />
+      <Product />
+    </>
+  );
+}
